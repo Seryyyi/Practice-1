d3097ed62b0172b5d15eaead83a162020a413a19
Seryyyi <riyuga13@gmail.com>
2023-02-28 19:52:19 +0300

    Practice 1

---
+++ Практика1 - переменные/Практика1 - пременные/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практика1___пременные
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte playerHealth = 100;
+            ushort inventorySize = 200;
+            uint playerDamage = 10;
+            int eatAmount = 15;
+            long waitingTime = 60;
+            float eatPrice = 5.5f;
+            double saleEatPrice = 4.4;
+            string playerName = "Genius";
+            char playerSymbol = '$';
+            bool playerAlive = true;
+        }
+    }
+}
